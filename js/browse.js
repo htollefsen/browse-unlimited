b4795485d1bf8e184869ccfb37a3eeecb4f2fbbe
Harald Tollefsen <harald@netmaking.no>
2014-08-19 22:24:44 +0200

    Changed responsemessage, fixed bug causing cleaning to stop halfway through

--- js/browse.js
+++ js/browse.js
@@ -1,8 +1,19 @@
-var debug=true;
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-53369842-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+var debug = false;
 var maxprogress = 100;   // total to reach
 var actualprogress = 0;  // current value
 var itv = 0;  // id to setinterval
-var mainId = 'browse-unlimited-loader';
+var divId = 'browse-unlimited-loader'; // div id to style
+var confirmationMessage = 'Happy Surfing!'; // onComplete message
 
 var domains=[
     "aftenposten.no",
@@ -31,7 +42,6 @@ $(document).ready(function(){
     resetElements();
     run();
     finished();
-    gaInit();
 });
 
 function addElement(id, parentId) {
@@ -39,7 +49,7 @@ function addElement(id, parentId) {
 }
 
 function resetElements() {
-    addElement('progress_wrapper', mainId);
+    addElement('progress_wrapper', divId);
     addElement('progressbar', 'progress_wrapper');
     addElement('indicator', 'progressbar');
 }
@@ -58,28 +68,13 @@ function run(){
 function removeCookie(cookie) {
     var url = "http" + (cookie.secure ? "s" : "") + "://" + cookie.domain + cookie.path;
     chrome.cookies.remove({"url": url, "name": cookie.name});
+    _gaq.push(['_trackEvent', 'Clearing', 'Cookie', url]);
     say('Removing ' + url + '.');
 }
 
 function finished() {
-    $('#' + mainId).html("OK!");
-}
-
-function gaInit() {
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-53369842-1']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-        var ga = document.createElement('script');
-        ga.type = 'text/javascript';
-        ga.async = true;
-        ga.src = 'https://ssl.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
-    })();
-
-    say('Tracking pageview');
+    $('#' + divId).width( 105 );
+    $('#' + divId).html(confirmationMessage);
 }
 
 function say(msg) {
